4174958b27648e72ba1e38a1a3044096d4bf5539
alsjoe <tyranthou@gmail.com>
2017-08-20 20:49:03 -0700

    feat: add CSS and icon
    Add basic CSS and icons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FaLeanpub from 'react-icons/lib/fa/leanpub'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="header">
+              <div className="grid row">
+              <h1 className='logo col-3'><FaLeanpub size={30}/> Readable</h1>
+              <ul className="navigation col-9">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Products</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+              </div>
+          </div>
+          <div className="container grid">
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
       </div>
     );
   }
